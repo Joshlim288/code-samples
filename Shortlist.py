4b7e79fe82c6105ae201a28ad22c2bbda5a87cae
Joshlim288 <47893380+Joshlim288@users.noreply.github.com>
2019-03-05 00:08:46 +0800

    Update Shortlist.py

--- Shortlist.py
+++ Shortlist.py
@@ -1,13 +1,19 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Jan 24 20:51:32 2019
+Created on Mon Mar  4 22:28:36 2019
 
+@author: JEOOOOOOOOOOOOSH
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 24 20:51:32 2019
 @author: Josh
 """
 
 import requests
 from bs4 import BeautifulSoup
-import csv
+import xlsxwriter
 
 
 def save_html(html, path):
@@ -19,15 +25,21 @@ def open_html(path):
     with open(path, 'rb') as f:
         return f.read()
 
-def write_csv(items)
-    with open('Shortlist.csv', 'a') as shortlist:
-        property_writer = csv.writer(shortlist)
-        property_writer.writerow(items)
+def write_xlsx(items, write_row):
+    write_column = 0
+    for item in items:
+        worksheet.write(write_row, write_column, item)
+        write_column += 1
+
+        
+
+workbook = xlsxwriter.Workbook('Results.xlsx')
+worksheet = workbook.add_worksheet()
 
 # user variables
 start_url = 'https://www.tripadvisor.com.sg/Hotels-g255100-Melbourne_Victoria-Hotels.html'
-num_rev_criteria = 7
-min_rev_num = 100
+num_rev_criteria = 10
+min_rev_num = 10
 min_star_rating = 4
 min_room_num = 90
 
@@ -36,7 +48,8 @@ r = requests.get(start_url)
 save_html(r.content, 'page')
 soup = BeautifulSoup(open_html('page'), 'html.parser')
 num_pages = int(soup.select_one('.pageNum.last.taLnk').text.strip())
-write_csv(['Property Name', 'Star Rating', 'Number of Rooms', 'Address', 'Contact Number'])
+write_row = 0
+write_xlsx(['Property Details', 'Star Rating', 'Number of Rooms'], write_row)
 
 # get property data
 for page_num in range(num_pages):
@@ -49,52 +62,74 @@ for page_num in range(num_pages):
         r = requests.get(property)
         save_html(r.content, 'page')
         soup = BeautifulSoup(open_html('page'), 'html.parser')
-        num_reviews = int(soup.select_one('.reviewCount').text.strip())
+        try:
+            num_reviews = int(soup.select_one('.reviewCount').text.strip().split(' ')[0].replace(',', ''))
+        except AttributeError:
+            num_reviews = 0
         if num_reviews >= min_rev_num:
             try:
                 property_name = soup.select_one('#HEADING').text.strip()
-            except TypeError:
+            except AttributeError:
                 property_name = ' '
 
             try:
-                star_rating_class = soup.select_one('.hotels-hotel-review-about-with-photos-layout-TextItem__textitem--3CMuR span')['class']
-                star_rating = int(star_rating_class[19] + '.' + star_rating_class[20])
+                star_rating_class = soup.select_one('.hotels-hotel-review-about-with-photos-layout-TextItem__textitem--3CMuR span')['class'][1]
+                star_rating = float(star_rating_class[5] + '.' + star_rating_class[6])
             except TypeError:
                 star_rating = 0
 
-            try:
-                num_rooms = int(soup.select_one('.hotels-hotel-review-about-addendum-AddendumItem__content--28NoV').text.strip())
-            except TypeError:
-                num_rooms = 0
+            num_rooms = 0
+            extra_info = soup.select('.hotels-hotel-review-about-addendum-AddendumItem__content--28NoV')
+            for data in extra_info:
+                data = data.text.strip()
+                if data.isdigit():
+                    num_rooms = int(data)
+
 
             try:            
                 address = soup.select_one('.street-address').text.strip() + ', ' + soup.select_one('.locality').text.strip() + ', ' + soup.select_one('.country-name').text.strip()
-            except TypeError:
+            except AttributeError:
                 address = ' '
             
             try:            
                 phone = soup.select_one('.is-hidden-mobile.detail').text.strip()
-            except TypeError:
+            except AttributeError:
                 phone = ' '
 
             if star_rating >= min_star_rating or star_rating == 0:
                 if num_rooms >= min_room_num or num_rooms == 0:
-                    write_csv([property_name, str(star_rating), str(num_rooms), address, phone])
+                    write_row += 1
+                    write_xlsx([property_name + '\n' + address + '\nT: ' + phone, star_rating, num_rooms], write_row)
         else:
             low_review_count += 1
-    next_page = soup.select_one('.nav.next.taLnk.ui_button.primary')['href']
-    page_url = 'https://www.tripadvisor.com.sg' + next_page
-    r = requests.get(page_url)
-    save_html(r.content,'page')
-    soup = BeautifulSoup(open_html('page'), 'html.parser')
-    if low_review_count > rev_criteria:
+    try:
+        next_page = soup.select_one('.nav.next.taLnk.ui_button.primary')['href']
+        page_url = 'https://www.tripadvisor.com.sg' + next_page
+        r = requests.get(page_url)
+        save_html(r.content,'page')
+        soup = BeautifulSoup(open_html('page'), 'html.parser')
+    except TypeError:
+        pass
+    if low_review_count >= num_rev_criteria:
         break
 
+workbook.close()
 
 
 
 '''
+Notes:
+1) Will still get data if any of the parameters are missing,
+   as long as the parameters that do exist meet the criteria
+2) Current break criteria is for a page to have a certain number
+   of entries with low reviews. This can be changed to suit needs.
+3) This scraper relies on data from tripadvisor, which might not
+   have much info on hotels in some destinations, like China.
+4) Address and phone number are taken from tripadvisor as well.
+   If you require time/dist to airport, and address/phone from google,
+   must be done manually. Google maps does not allow these pages to
+   be scraped.
+   
 ToDo:
-
-
+1) Replace try and excepts with something less problematic
 '''
