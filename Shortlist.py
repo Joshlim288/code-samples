6eec467b6b44277d8a50845f371905a96222e3cd
Joshlim288 <47893380+Joshlim288@users.noreply.github.com>
2019-03-05 15:20:37 +0800

    Update Shortlist.py

--- Shortlist.py
+++ Shortlist.py
@@ -4,12 +4,6 @@ Created on Mon Mar  4 22:28:36 2019
 @author: JEOOOOOOOOOOOOSH
 """
 
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Jan 24 20:51:32 2019
-@author: Josh
-"""
-
 import requests
 from bs4 import BeautifulSoup
 import xlsxwriter
@@ -24,13 +18,13 @@ def open_html(path):
     with open(path, 'rb') as f:
         return f.read()
 
+
 def write_xlsx(items, write_row):
     write_column = 0
     for item in items:
         worksheet.write(write_row, write_column, item)
         write_column += 1
 
-        
 
 workbook = xlsxwriter.Workbook('Results.xlsx')
 worksheet = workbook.add_worksheet()
@@ -43,39 +37,41 @@ while True:
     else:
         break
 
-
 while True:
     min_rev_num = input('Min Reviews for property: ')
     if min_rev_num.isdigit():
         if int(min_rev_num) >= 0:
+            min_rev_num = int(min_rev_num)
             break
     print('Please enter a valid number')
-
 
 while True:
     print('Enter max number of low review number properties on a single page, from 0 to 30.')
     print('(Program will exit once this condition is fufilled)')
     num_rev_criteria = input('Input: ')
     if num_rev_criteria.isdigit():
-        if 0 <= num_rev_criteria <= 30:
+        if 0 <= int(num_rev_criteria) <= 30:
+            num_rev_criteria = int(num_rev_criteria)
             break
-    print('Please enter a valid number')
 
+    print('Please enter a valid number')
 
 while True:
     min_star_rating = input('Min star rating for property: ')
     if min_star_rating.isdigit():
         if 0 <= int(min_star_rating) <= 5:
+            min_star_rating = float(min_star_rating)
             break
-    print('Please enter a valid number')
 
+    print('Please enter a valid number')
 
 while True:
     min_room_num = input('Min number of rooms: ')
     if min_room_num.isdigit():
-        if min_room_num >= 0:
+        if int(min_room_num) >= 0:
+            min_room_num = int(min_room_num)
             break
-
+    print('Please enter a valid number')
 
 # get num pages
 r = requests.get(start_url)
@@ -84,16 +80,27 @@ soup = BeautifulSoup(open_html('page'), 'html.parser')
 num_pages = int(soup.select_one('.pageNum.last.taLnk').text.strip())
 write_row = 0
 write_xlsx(['Property Details', 'Star Rating', 'Number of Rooms'], write_row)
+page_url = start_url
 
+print('Getting data...')
 # get property data
 for page_num in range(num_pages):
+    print('On page {}'.format(str(page_num+1)))
     low_review_count = 0
+    r = requests.get(page_url)
+    save_html(r.content, 'page')
+    soup = BeautifulSoup(open_html('page'), 'html.parser')
+    if page_num != num_pages - 1:
+        next_page = soup.select_one('.nav.next.taLnk.ui_button.primary')['href']
+        page_url = 'https://www.tripadvisor.com.sg' + next_page
+    else:
+        pass
     rows = soup.select('.property_title.prominent')
     prop_urls = []
     for row in rows:
         prop_urls.append('https://www.tripadvisor.com.sg' + row['href'])
-    for property in prop_urls:
-        r = requests.get(property)
+    for prop in prop_urls:
+        r = requests.get(prop)
         save_html(r.content, 'page')
         soup = BeautifulSoup(open_html('page'), 'html.parser')
         try:
@@ -119,13 +126,13 @@ for page_num in range(num_pages):
                 if data.isdigit():
                     num_rooms = int(data)
 
-
-            try:            
-                address = soup.select_one('.street-address').text.strip() + ', ' + soup.select_one('.locality').text.strip() + ', ' + soup.select_one('.country-name').text.strip()
+            try:
+                address = soup.select_one('.street-address').text.strip() + ', ' + soup.select_one(
+                    '.locality').text.strip() + ', ' + soup.select_one('.country-name').text.strip()
             except AttributeError:
                 address = ' '
-            
-            try:            
+
+            try:
                 phone = soup.select_one('.is-hidden-mobile.detail').text.strip()
             except AttributeError:
                 phone = ' '
@@ -136,20 +143,11 @@ for page_num in range(num_pages):
                     write_xlsx([property_name + '\n' + address + '\nT: ' + phone, star_rating, num_rooms], write_row)
         else:
             low_review_count += 1
-    if page_num != num_pages - 1:
-        next_page = soup.select_one('.nav.next.taLnk.ui_button.primary')['href']
-        page_url = 'https://www.tripadvisor.com.sg' + next_page
-        r = requests.get(page_url)
-        save_html(r.content,'page')
-        soup = BeautifulSoup(open_html('page'), 'html.parser')
-    else:
-        pass
+
     if low_review_count >= num_rev_criteria:
         break
-
+print('Done!')
 workbook.close()
-
-
 
 '''
 Notes:
@@ -163,9 +161,10 @@ Notes:
    If you require time/dist to airport, and address/phone from google,
    must be done manually. Google maps does not allow these pages to
    be scraped.
-   
+
 ToDo:
 1) Replace try and excepts with something less problematic
 2) include vba script for formatting
 3) Get inputs  through tkinter
+4) Work on efficiency*
 '''
