403f05b9f2c744faeafdf925593247bbb5496b62
Joshlim288 <47893380+Joshlim288@users.noreply.github.com>
2019-03-19 15:10:05 +0800

    Update CarousellUpdater.py

--- CarousellUpdater.py
+++ CarousellUpdater.py
@@ -14,28 +14,38 @@ def save_html(html, path):
 def open_html(path):
     with open(path, 'rb') as i:
         return i.read()
-
-# Variables
-url = 'https://sg.carousell.com/search/products/?sort_by=time_created%2Cdescending&query=nintendo%20switch%20games&cc_id=412'
-delay = 5 # in minutes
-
-# Get current first sale
+    
+check = input('close script when new listing found?').upper()
 print('Getting data...\n')
-r = requests.get(url)
-soup = BeautifulSoup(r.content, 'html.parser')
-current_first_sale = soup.select_one('.G-ab').text.strip()
 
-# Check page
 while True:
-    # save pages
-    print('Refreshing...\n')
+    # Variables
+    url = 'https://sg.carousell.com/search/products/?sort_by=time_created%2Cdescending&query=nintendo%20switch%20games&cc_id=412'
+    delay = 5 # in minutes
+
+    # Get current first sale
     r = requests.get(url)
     soup = BeautifulSoup(r.content, 'html.parser')
-    new_first_sale = soup.select_one('.G-ab').text.strip()
-    if new_first_sale != current_first_sale:
-        break
-    print('{}: No new updates\n'.format(datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
-    time.sleep(delay*60)
+    current_first_sale = soup.select_one('.G-ab')
 
-url = 'https://sg.carousell.com' + new_first_sale['href']
-webbrowser.open(url)
+    # Check page
+    while True:
+        # save pages
+        print('Refreshing...\n')
+        r = requests.get(url)
+        soup = BeautifulSoup(r.content, 'html.parser')
+        new_first_sale = soup.select_one('.G-ab')
+        if new_first_sale.text.strip() != current_first_sale.text.strip():
+            print('New listing found: {}'.format(new_first_sale.text.strip()))
+            print('Opening web page...')
+            break
+        print('{}: No new updates\n'.format(datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
+        time.sleep(delay*60)
+
+    url = 'https://sg.carousell.com' + new_first_sale['href']
+    webbrowser.open(url)
+    if check == 'Y':
+        break
+    
+    
+
